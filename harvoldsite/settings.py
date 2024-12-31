41053c1ea1f6aff15b196ad4824fbf0c41e640a1
Yidi Wu <yidi.wu99@gmail.com>
2024-12-31 17:25:51 -0500

    fuck mobile users

--- harvoldsite/settings.py
+++ harvoldsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z*v5op0lmp!8d!7gtmnlbu%2g8wge6tksn_kwlp$svn573zolr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
