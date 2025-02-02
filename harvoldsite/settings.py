5a98010cefae1ea4441385054c8e70bb1d9e1237
YidiWuCE21 <yidi.wu99@gmail.com>
2025-02-02 13:36:49 -0500

    stuff

--- harvoldsite/settings.py
+++ harvoldsite/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "https://harvold-alpha-c1e3d777167f.herokuapp.com"]
 
@@ -91,7 +92,7 @@ WSGI_APPLICATION = 'harvoldsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if DEBUG:
+if False:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
