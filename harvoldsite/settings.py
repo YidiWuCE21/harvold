59e5d0e7b69ef4d099f6a3eb8817bca98dd1a210
YidiWuCE21 <yidi.wu99@gmail.com>
2025-02-02 13:29:54 -0500

    stuff

--- harvoldsite/settings.py
+++ harvoldsite/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
 
 ALLOWED_HOSTS = ["*", "https://harvold-alpha-c1e3d777167f.herokuapp.com"]
 
